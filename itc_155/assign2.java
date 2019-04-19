43b0cbde1466fbfa018a7a4485cbfc032862516a
Jess <jessallen6@hotmail.com>
2019-04-19 15:38:59 -0700

    Create assign2.java
    code for Assignment 2

---
+++ itc_155/assign2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+
+public class assign2 {
+	
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			for (int i = 0; i < stopWords.size(); i++) {
+				String words = array.get(i);
+				if (word.length() % 2 == 0) {
+					array.remove(i);
+					i--;
+				}
+			}
+		}
+		
+		
+	}
